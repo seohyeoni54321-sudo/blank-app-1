a9a0f8476878d1d165d58b053f9020565aa9d05e
seohyeoni54321-sudo <seohyeoni54321@gmail.com>
2025-11-07 05:45:42 +0000

    코드수정03

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,7 +92,7 @@ def ask_agent(agent_executor, question: str):
 # --------------------------------------------------------------------
 def main():
     st.set_page_config(page_title="기술보증기금 AI 비서", layout="wide", page_icon="🤖")
-    st.image('data/kibo_image.jpg', width=800)
+    st.image('data/빅히어로.png', width=800)
     st.markdown('---')
     st.title("안녕하세요! RAG + Web을 활용한 '기술보증기금 AI 비서' 입니다")  
 
